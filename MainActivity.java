9ffb1e5f079acbc523fb5f43c4e74e49415bf51c
Tasnuva Tabassum Oshin <tasnuva.oshin12@gmail.com>
2019-07-31 23:45:00 +0600

    market file

---
+++ MainActivity.java
@@ -0,0 +1,54 @@
+package com.joytechnologies.market;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import com.joytechnologies.market.Market.Market_Home_Fragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Market_Home_Fragment market_home_fragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        market_home_fragment = new Market_Home_Fragment();
+        /*
+        if permission granted then we will go the map activity
+         */
+
+            SetFragment(market_home_fragment);
+
+    }
+
+
+
+    private void SetFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.addToBackStack("my_fragment").commit();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (getFragmentManager().getBackStackEntryCount() > 0) {
+            getFragmentManager().popBackStack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+
+}
---
+++ Market/Market_Home_Fragment.java
@@ -0,0 +1,435 @@
+package com.joytechnologies.market.Market;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.joytechnologies.market.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+
+public class Market_Home_Fragment extends Fragment implements OnMapReadyCallback{
+
+    private static final int MY_PERMISSIONS_REQUEST_LOCATION = 99 ;
+
+    GoogleMap gMap;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private Location location;
+    private Double currentLang,currentLong;
+    Marker mCurrLocationMarker;
+/*
+need to implement method for map ready and getting the current location from the map
+ */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View view = inflater.inflate(R.layout.fragment_market__home_, container, false);
+
+       /*
+           We need to initialize the map for our work
+        */
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.f_map);
+        supportMapFragment.getMapAsync(this);
+
+
+        return view;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+       if(checkLocationPermission()) {
+           gMap = googleMap;
+           gMap.setMyLocationEnabled(true);
+           //for getting the current Location for my place
+           GetCurrentLocation();
+       }
+
+    }
+
+    //current Location Method to get the current Location Value
+    /*
+    without current Location we cant get the current long and lang we need this for nearby
+    market from my place
+     */
+    private void GetCurrentLocation() {
+        if (checkLocationPermission()) {
+            Log.d("debug", "Now We want to show our current location");
+            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+            @SuppressLint("MissingPermission") Task LocationTask = fusedLocationProviderClient.getLastLocation();
+
+            LocationTask.addOnCompleteListener(new OnCompleteListener() {
+
+                @Override
+                public void onComplete(@NonNull Task task) {
+                    if (task.isSuccessful()) {
+
+                        Log.d("debug", "Now We get our Current Location");
+
+                        location = (Location) task.getResult();
+
+                        currentLang = location.getLatitude();
+                        currentLong = location.getLongitude();
+                        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 15f));
+                        Log.d("lat", String.valueOf(currentLang));
+                        Log.d("long", String.valueOf(currentLong));
+
+
+
+
+
+                        // UpdateCamera(new LatLng(location.getLatitude(), location.getLongitude()), DEFAULT_ZOOM, "Location");
+                          /*
+                          now we will get the api from which we can the nearby market
+                           */
+                        StringBuilder sbValue = new StringBuilder(GetNearByApi(currentLang,currentLong));
+                        PlacesTask placesTask = new PlacesTask();
+                        placesTask.execute(sbValue.toString());
+                       // placesTask.execute("http://v-tube.xyz/market/test.php");
+
+
+                    }
+                }
+            });
+        }
+
+    }
+
+    //for checking the location Permission
+
+//checking the permission before the full process starts
+
+
+
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                new AlertDialog.Builder(getActivity())
+                        .setTitle("Permission")
+                        .setMessage("Please Share/on Your Location")
+                        .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(getActivity(),
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                        MY_PERMISSIONS_REQUEST_LOCATION);
+                            }
+                        })
+                        .create()
+                        .show();
+
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+
+    //this is for the nearby Location APi
+
+
+    public StringBuilder GetNearByApi(Double CurrentLatitde, Double CurrentLongitude){
+        //use your current location here
+//        double mLatitude = 23.782439;
+//        double mLongitude = 90.415560;
+
+
+        double mLatitude = CurrentLatitde;
+        double mLongitude = CurrentLongitude;
+        StringBuilder api = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+        api.append("location=" + mLatitude + "," + mLongitude);
+        api.append("&radius=5000");
+        api.append("&types=" +"shopping_mall");
+        api.append("&sensor=true");
+        api.append("&key=AIzaSyAycZ7yEq_SjhoFHcq60NptoLBTN-f2lwc"); //key this is important we need to add the nearby location api
+        Log.d("Map", "url: " + api.toString());
+
+        return api;
+    }
+
+     //all the method for place api fetching
+
+
+    private class PlacesTask extends AsyncTask<String, Integer, String>
+    {
+
+        String data = null;
+
+        // Invoked by execute() method of this object
+        @Override
+        protected String doInBackground(String... url) {
+            try {
+                data = downloadUrl(url[0]);
+            } catch (Exception e) {
+                Log.d("Background Task", e.toString());
+            }
+            return data;
+        }
+
+        // Executed after the complete execution of doInBackground() method
+        @Override
+        protected void onPostExecute(String result) {
+            ParserTask parserTask = new ParserTask();
+
+            // Start parsing the Google places in JSON format
+            // Invokes the "doInBackground()" method of the class ParserTask
+            parserTask.execute(result);
+        }
+    }
+
+    private String downloadUrl(String strUrl) throws IOException
+    {
+        String data = "";
+        InputStream iStream = null;
+        HttpURLConnection urlConnection = null;
+        try {
+            URL url = new URL(strUrl);
+
+            // Creating an http connection to communicate with url
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            // Connecting to url
+            urlConnection.connect();
+
+            // Reading data from url
+            iStream = urlConnection.getInputStream();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
+
+            StringBuffer sb = new StringBuffer();
+
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+
+            data = sb.toString();
+
+            br.close();
+
+        } catch (Exception e) {
+            Log.d("Exception", e.toString());
+        } finally {
+            iStream.close();
+            urlConnection.disconnect();
+        }
+        return data;
+    }
+
+    private class ParserTask extends AsyncTask<String, Integer, List<HashMap<String, String>>> {
+
+        JSONObject jObject;
+
+        // Invoked by execute() method of this object
+        @Override
+        protected List<HashMap<String, String>> doInBackground(String... jsonData) {
+
+            List<HashMap<String, String>> places = null;
+            Place_JSON placeJson = new Place_JSON();
+
+            try {
+                jObject = new JSONObject(jsonData[0]);
+
+                places = placeJson.parse(jObject);
+
+            } catch (Exception e) {
+                Log.d("Exception", e.toString());
+            }
+            return places;
+        }
+
+        // Executed after the complete execution of doInBackground() method
+        @Override
+        protected void onPostExecute(List<HashMap<String, String>> list) {
+
+            Log.d("Map", "list size: " + list.size());
+            // Clears all the existing markers;
+            gMap.clear();
+
+            for (int i = 0; i < list.size(); i++) {
+
+                // Creating a marker
+                MarkerOptions markerOptions = new MarkerOptions();
+
+                // Getting a place from the places list
+                HashMap<String, String> hmPlace = list.get(i);
+
+
+                // Getting latitude of the place
+                double lat = Double.parseDouble(hmPlace.get("lat"));
+
+                // Getting longitude of the place
+                double lng = Double.parseDouble(hmPlace.get("lng"));
+
+                // Getting name
+                String name = hmPlace.get("place_name");
+
+                Log.d("Map", "place: " + name);
+
+                // Getting vicinity
+                String vicinity = hmPlace.get("vicinity");
+
+                LatLng latLng = new LatLng(lat, lng);
+
+                // Setting the position for the marker
+                markerOptions.position(latLng);
+
+                markerOptions.title(name + " : " + vicinity);
+
+                markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
+
+                // Placing a marker on the touched position
+                Marker m = gMap.addMarker(markerOptions);
+
+                //for showing the current Position ALso
+                LatLng latLngs= new LatLng(location.getLatitude(), location.getLongitude());
+                MarkerOptions markerOptions2 = new MarkerOptions();
+                markerOptions2.position(latLngs);
+                markerOptions2.title("Current Position");
+                markerOptions2.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+                mCurrLocationMarker = gMap.addMarker(markerOptions2);
+
+
+            }
+        }
+    }
+    public class Place_JSON {
+
+        /**
+         * Receives a JSONObject and returns a list
+         */
+        public List<HashMap<String, String>> parse(JSONObject jObject) {
+
+            JSONArray jPlaces = null;
+            try {
+                /** Retrieves all the elements in the 'places' array */
+                jPlaces = jObject.getJSONArray("results");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            /** Invoking getPlaces with the array of json object
+             * where each json object represent a place
+             */
+            return getPlaces(jPlaces);
+        }
+
+        private List<HashMap<String, String>> getPlaces(JSONArray jPlaces) {
+            int placesCount = jPlaces.length();
+            List<HashMap<String, String>> placesList = new ArrayList<HashMap<String, String>>();
+            HashMap<String, String> place = null;
+
+            /** Taking each place, parses and adds to list object */
+            for (int i = 0; i < placesCount; i++) {
+                try {
+                    /** Call getPlace with place JSON object to parse the place */
+                    place = getPlace((JSONObject) jPlaces.get(i));
+                    placesList.add(place);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            return placesList;
+        }
+
+        /**
+         * Parsing the Place JSON object
+         */
+        private HashMap<String, String> getPlace(JSONObject jPlace)
+        {
+
+            HashMap<String, String> place = new HashMap<String, String>();
+            String placeName = "-NA-";
+            String vicinity = "-NA-";
+            String latitude = "";
+            String longitude = "";
+            String reference = "";
+
+            try {
+                // Extracting Place name, if available
+                if (!jPlace.isNull("name")) {
+                    placeName = jPlace.getString("name");
+                }
+
+                // Extracting Place Vicinity, if available
+                if (!jPlace.isNull("vicinity")) {
+                    vicinity = jPlace.getString("vicinity");
+                }
+
+                latitude = jPlace.getJSONObject("geometry").getJSONObject("location").getString("lat");
+                longitude = jPlace.getJSONObject("geometry").getJSONObject("location").getString("lng");
+                reference = jPlace.getString("reference");
+
+                place.put("place_name", placeName);
+                place.put("vicinity", vicinity);
+                place.put("lat", latitude);
+                place.put("lng", longitude);
+                place.put("reference", reference);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return place;
+        }
+    }
+
+
+}
